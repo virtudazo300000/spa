57e5f5fb28c32f0f6aadbc5082eb68b91956eb4a
Your Name <you@example.com>
2025-05-10 05:59:47 +0000

    WIP: save changes before rebase

--- script/app.js
+++ script/app.js
@@ -1,4 +1,21 @@
+<<<<<<< HEAD
 Alpine.store("settings", {
     apiBaseUrl:"http://172.17.100.14:3383/default/api/",  
     appName: "Ecommerce",  
 });
+=======
+
+Alpine.store("settings", {
+    apiBaseUrl:"http://172.17.100.14:3329/default/api",  
+    appName: "contact application",  
+   
+});
+
+Alpine.store("settings", {
+    apiBaseUrl:"http://172.17.100.14:3329/default/api/exam",  
+    appName: "contact application",  
+   
+});
+
+
+>>>>>>> 97bb422... first commit
